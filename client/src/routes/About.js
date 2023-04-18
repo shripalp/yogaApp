e194e13f0a38c95d8dee5854b17967aa9e2c0445
shripalp <67804521+shripalp@users.noreply.github.com>
2023-04-18 21:09:48 +0000

    about me fixed

--- client/src/routes/About.js
+++ client/src/routes/About.js
@@ -2,27 +2,22 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import PDF from "../components/PDF2";
-import Image from "react-bootstrap/Image";
+//import Image from "react-bootstrap/Image";
 import CustomCard from "../components/CustomCard";
 
 import { Container, Col, Row } from "react-bootstrap";
 
 function About(props) {
-  //const image2 = require("../components/assets/lotus.jpg");
   const shripalInfo = {
     title: "Shripal Parikh",
     subtitle: "Yoga Teacher, YA-200h",
-    text: "Yoga Teacher",
+    text: "Shripal is teaching yoga for past 10 years. He is trained in Iyengar style of yoga and SriSri yoga and has completed 200 hours yoga training with both.  He teaches yoga to all age groups.",
     width: 600,
     url: "/Contact",
     img: require("../components/assets/shripal1.jpg"),
     link: "Contact Shripal",
   };
-  // const title = "yoga";
-  // const text = "yoga is good";
-  // const width = 100;
-  // const url = "/Contact";
-  // const image = require("../components/assets/lotus.jpg");
+
   return (
     <Container fluid>
       <Row>
@@ -30,35 +25,29 @@ function About(props) {
           <Header token={props.token} />
         </Col>
       </Row>
-      <Row className="mt-5">
-        <Col md={6}>
-          <PDF
-            file={require("../components/assets/SSY_certificate.pdf")}
-            scale={0.4}
-          />
-        </Col>
-        <Col>
+      <Row className="mt-5 d-flex flex-wrap">
+        <Col lg={4} md={6}>
           <CustomCard {...shripalInfo} />
         </Col>
-        <Col md={6}>
-          <Row>
-            <Col>
-              <Image
-                className="cert"
-                src={require("../components/assets/AYC_certificate.jpg")}
-                width="300"
-              />
-            </Col>
-          </Row>
-          <Row className="mt-5">
-            <Col>
-              <PDF
-                file={require("../components/assets/RegistryCard.pdf")}
-                scale={0.3}
-              />
-            </Col>
-          </Row>
+        {/* <Col>
+          <PDF
+            file={require("../components/assets/SSY_certificate.pdf")}
+            scale={0.3}
+          />
+        </Col> */}
+        <Col ld={4} md={6}>
+          <PDF
+            scale={0.7}
+            file={require("../components/assets/RegistryCard.pdf")}
+          />
         </Col>
+        {/* <Col>
+          <Image
+            className="ayccert"
+            src={require("../components/assets/AYC_certificate.jpg")}
+            width={600}
+          />
+        </Col> */}
       </Row>
 
       <Row>
