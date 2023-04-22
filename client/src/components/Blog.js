ea5ebdec91195b03f5cd49b81b6c9ac641c080d2
shripal <shripalp@gmail.com>
2023-04-22 09:23:36 -0600

    image added

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -14,6 +14,7 @@ const Blog = (props) => {
         title={props.blog.title}
         subtitle={props.blog.author}
         text={props.blog.content}
+        img={props.blog.image}
       />
 
       {props.token ? (
