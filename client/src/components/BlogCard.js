302d9a42e86862e77b79d105eea8f75b4ec1d398
shripal <shripalp@gmail.com>
2023-04-24 22:11:34 -0600

    Navigate component added

--- client/src/components/BlogCard.js
+++ client/src/components/BlogCard.js
@@ -1,11 +1,13 @@
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import blogService from "../services/blogService";
+import React from "react";
 //import CustomCard from "./CustomCard";
 import Card from "react-bootstrap/Card";
 
 const BlogCard = (props) => {
+  const { click, setClick } = React.useState(false);
   const styles = {
     card: {
       backgroundColor: "#B7E0F2",
@@ -32,12 +34,14 @@ const BlogCard = (props) => {
     event.preventDefault();
     blogService.remove(props.blog._id);
   };
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate("/blog", { replace: true });
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setClick(true);
   };
   return (
     <Col lg={4} md={4} sm={6} xs={12}>
+      {click && <Navigate to="/blog" replace={true} />}
       <Card className="m-1" bg="light" style={styles.card}>
         <Card.Img
           variant="top"
@@ -50,7 +54,7 @@ const BlogCard = (props) => {
             {props.blog.subtitle}
           </Card.Subtitle>
           <Card.Text style={styles.cardText}>{props.blog.text}</Card.Text>
-          <Button onclick={handleClick} variant="primary">
+          <Button onclick={handleClick} type="submit" variant="primary">
             Go somewhere
           </Button>
           <Card.Link href="/contact">contact me</Card.Link>
